8b15c2f539cb2b6b7ba92225d8a8576a68f3f8a6
pr0xius <mannankau74@gmail.com>
2025-04-01 01:51:14 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
-int main(){
-    int n;
-    scanf("%dd",&n);
-    int arr[n],hash[100]={0};
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-        hash[arr[i]]=1;
-    }
-    int longest=0,curr=0;
-    for (int i=0;i<100;i++){
-        if(hash[i]){
-            curr++;
-            if(curr>longest) longest=curr;
-            else curr=0;
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-    printf("%d",longest);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubbleSort(arr, n);
+    int longest = 1, current = 1;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) continue; 
+        if (arr[i] == arr[i - 1] + 1) {
+            current++;
+        } else {
+            if (current > longest) longest = current;
+            current = 1;
+        }
+    }
+    if (current > longest) longest = current;
+    printf("%d\n", longest);
     return 0;
 }
